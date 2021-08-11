7fb419cd2dab1655ad7d155361d10f21df5f5b1e
Tristan Tinning <tristan_tinning@hotmail.com>
2021-08-11 14:40:22 +0100

    merged restuatnts with navbar

--- Late_Tables/screens/Home.js
+++ Late_Tables/screens/Home.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, {useState, useEffect} from "react"
 import{
     View,
     Text,
@@ -8,17 +8,34 @@ import{
     Image,
     FlatList,
     Touchable,
-    ColorPropType
+    ColorPropType,
+    ScrollView,
+    Linking
 } from "react-native"
 import { sin } from "react-native-reanimated"
 import { icons, images, SIZES, COLORS, FONTS } from "../constants"
+import Accordion from 'react-native-collapsible/Accordion'
+
 
 
 const Home = () => {
 
 
-    const [selectedCategory, setSelectedCategory] = React.useState(null)
+    const [info, setInfo] = useState([])
+    const [activeSections, setActiveSections] = useState([])
 
+    const getRestaurants = async () => {
+        try{
+            const response = await fetch('http://localhost:8080/restaurants');
+            const json = await response.json();
+            setInfo(json)
+            console.log(info)
+            console.log("test")
+        }
+        catch(error){
+            console.error(error)
+        }
+    }
 
 
     function renderHeader() {
@@ -77,17 +94,62 @@ const Home = () => {
         )
     }
 
+    renderRestaurantHeader = (section) => {
+        return (
+            <View style={styles.restaurantItem}>
+                <Image 
+                    source={{
+                    width: "90%",
+                    height: 100,
+                    uri: section.mainImage}}
+                ></Image>
+                <Text style={styles.items}>{section.name}</Text>
+            </View>
+            )
+        }
+        
+        renderRestaurantContent = (section) => {
+            return (
+            <View>
+                <Text>{section.address}</Text>
+                <Text>{section.telephoneNumber}</Text>
+            </View>
+            )
+        }
+        
+        updateSections = (activeSections) => {
+            setActiveSections(activeSections)
+        }
+    
+
+        useEffect(() => {
+            getRestaurants()
+            },[])
+
     return (
         <SafeAreaView style={styles.container}>
             {renderHeader()}
+            <ScrollView contentContainerStyle={styles.scrollContainer}>
+                <Accordion
+                    sections={info}
+                    activeSections={activeSections}
+                    renderHeader={renderRestaurantHeader}
+                    renderContent={renderRestaurantContent}
+                    onChange={updateSections}
+                />
+            </ScrollView>
         </SafeAreaView>
     )
 }
 
+
+
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: COLORS.lightGray4
+        backgroundColor: COLORS.lightGray4,
+        alignItems: 'center',
+        justifyContent: 'center',
     },
     shadow: {
         shadowColor: "#000",
@@ -98,6 +160,10 @@ const styles = StyleSheet.create({
         shadowOpacity: 0.1,
         shadowRadius: 3,
         elevation: 1,
+    },
+    restaurantItem: {
+        flex: 1,
+        alignItems: 'center',
     }
 })
 
