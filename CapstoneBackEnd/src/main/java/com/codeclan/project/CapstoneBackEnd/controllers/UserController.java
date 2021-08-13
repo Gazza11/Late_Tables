1aa9a49e8e8a3ac1966e9e090c85df8d69267325
Zaki Yusaf <zakiyusaf@hotmail.co.uk>
2021-08-13 14:12:06 +0100

    Updated user controller to allow update of details and also added in create and delete routes however left them commented out for now and minor fix to dataloader changing outsider address and ikigai name

--- CapstoneBackEnd/src/main/java/com/codeclan/project/CapstoneBackEnd/controllers/UserController.java
+++ CapstoneBackEnd/src/main/java/com/codeclan/project/CapstoneBackEnd/controllers/UserController.java
@@ -5,11 +5,10 @@ import com.codeclan.project.CapstoneBackEnd.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 
 @RestController
 public class UserController {
@@ -26,5 +25,33 @@ public class UserController {
     public ResponseEntity getUserById(@PathVariable Long id){
         return new ResponseEntity(userRepository.findById(id), HttpStatus.OK);
     }
+
+//    @PostMapping(value = "/users")
+//    public ResponseEntity createUser(@RequestBody User user){
+//        userRepository.save(user);
+//        return new ResponseEntity(user, HttpStatus.CREATED);
+//    }
+
+    @PutMapping(value = "/users/{id}")
+    public ResponseEntity updateUser(@PathVariable Long id, @RequestBody User user){
+        try {
+            User userToUpdate = userRepository.findById(id).get();
+            userToUpdate.setName(user.getName());
+            userToUpdate.setUsername(user.getUsername());
+            userToUpdate.setEmail(user.getEmail());
+
+            userRepository.save(userToUpdate);
+            return new ResponseEntity(userToUpdate, HttpStatus.OK);
+        }
+        catch (NoSuchElementException ex){
+            return new ResponseEntity("user not found", HttpStatus.NOT_FOUND);
+        }
+    }
+
+//    @DeleteMapping(value = "/users/{id}")
+//    public ResponseEntity deleteUser(@PathVariable Long id){
+//        userRepository.deleteById(id);
+//        return new ResponseEntity(id, HttpStatus.OK);
+//    }
 
 }
